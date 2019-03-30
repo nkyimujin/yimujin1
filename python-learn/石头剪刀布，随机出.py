43ec0623e21393e0ec21f13e17193668c63190c1
yimujin <ssida1@outlook.com>
2019-03-30 14:49:05 +0800

    Update 石头剪刀布，随机出.py

--- python-learn/石头剪刀布，随机出.py
+++ python-learn/石头剪刀布，随机出.py
@@ -3,7 +3,6 @@ import time
 
 player = int(random.randint(1, 3))  # round(int(input('请输入您要出的拳头：石头=1，剪刀=2，布=3:')))
 computer = int(random.randint(1, 3))
-# print(time.clock())
 print(time.process_time())
 if (player < 1 or computer < 1
         or player > 3 or computer > 3):
@@ -26,7 +25,6 @@ elif player == computer and player and computer > 0:
         print(a)
     else:
         print('都出的布，平局')
-    # print('平局了呢,电脑出的是%s,您出的是%s' % (computer, player))
 else:
     if player == 1:
         print('您出的石头，电脑出的布，很遗憾')
@@ -34,6 +32,4 @@ else:
         print('您出的剪刀，但是电脑出的布，很遗憾')
     else:
         print('电脑出的布，您出的石头，很可惜')
-    # print('连电脑都打不过，渣渣,电脑出的是%s,您出的是%s' % (computer, player))
-# print(time.clock())
 print(time.process_time())
